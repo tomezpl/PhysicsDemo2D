fc19c4b9894b07d2edeebf89111f2ad124e75dea
Tomasz Zając <tomezpl@gmail.com>
2020-10-17 19:46:17 +0100

    Adds basic gravity physics

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,53 @@
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
 
+#include <cmath>
+
+#define GRAVITY 9.81f
+
+class Vec2 
+{
+public:
+	float x, y;
+	Vec2(float X = .0f, float Y = .0f) { x = X; y = Y; }
+
+	Vec2 operator+(Vec2 rh) { return Vec2(x + rh.x, y + rh.y); }
+	Vec2 operator-() { return Vec2(-x, -y); }
+	Vec2 operator-(Vec2 rh) { return Vec2(x - rh.x, y - rh.y); }
+	Vec2 operator/(float rh) { return rh == .0f ? Vec2() : Vec2(x / rh, y / rh); }
+	Vec2 operator*(float rh) { return Vec2(x * rh, y * rh); }
+	void operator+=(Vec2 rh) { *this = *this + rh; }
+	void operator-=(Vec2 rh) { *this += -rh; }
+	void operator*=(float rh) { *this = *this * rh; }
+	void operator/=(float rh) { *this = *this / rh; }
+
+	float magnitude() { return sqrtf(x * x + y * y); }
+	Vec2 normalised() { return *this / magnitude(); }
+};
+
+class RBDynamic 
+{
+public:
+	RBDynamic(Vec2 Position = Vec2(), float Mass = 1.f) 
+	{
+		position = Position;
+		mass = Mass;
+		velocity = Vec2();
+	}
+
+	// Centre of mass position
+	Vec2 position;
+	float mass;
+	Vec2 velocity;
+
+	void fall(float time) 
+	{
+		velocity += Vec2(.0f, GRAVITY) * mass;
+
+		position += velocity * time;
+	}
+};
+
 int main()
 {
 	al_init();
@@ -15,7 +62,12 @@ int main()
 	
 	ALLEGRO_EVENT* ev = new ALLEGRO_EVENT();
 
+	RBDynamic box = RBDynamic(Vec2(640.f, 360.f));
+	float boxSize = 60.f;
+	float boxHx = boxSize / 2.f; // Box half-extents
+
 	bool isRunning = true;
+	float frameStartTime = al_get_time();
 	while (isRunning)
 	{
 		while (al_get_next_event(evQueue, ev))
@@ -30,8 +82,12 @@ int main()
 
 		al_clear_to_color(al_map_rgb(100, 149, 237));
 
-		al_draw_filled_rectangle(610, 330, 670, 390, al_map_rgb(255, 0, 0));
+		// I'M GONNA FUCKING FALL
+		box.fall(al_get_time() - frameStartTime);
+		al_draw_filled_rectangle(box.position.x - boxHx, box.position.y - boxHx, box.position.x + boxHx, box.position.y + boxHx, al_map_rgb(255, 0, 0));
 
 		al_flip_display();
+
+		frameStartTime = al_get_time();
 	}
 }
